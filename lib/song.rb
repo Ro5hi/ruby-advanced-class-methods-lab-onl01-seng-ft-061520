49b369751247b9af09e0ed9b15c5a6e85b1125ee
Shirlen Detablan <slen.detablan@gmail.com>
2020-06-22 20:31:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,7 +31,14 @@ class Song
   def self.find_or_create_by_name
     @@all.find(name) || self.create(name) 
   end 
- 
+  
+  def self.new_from_filename(name)
+    song = self.new 
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+    song
+  end
+  
   def self.create_from_filename(name)
     song = self.new
     song.name = (name.split(" - ")[1].chomp(".mp3"))
