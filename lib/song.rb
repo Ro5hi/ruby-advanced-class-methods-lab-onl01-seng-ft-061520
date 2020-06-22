0c372e0a0287a786f8512fe72a9da020f3b8e49f
Shirlen Detablan <slen.detablan@gmail.com>
2020-06-22 20:15:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,3 +11,36 @@
 #   end
 
 # end
+
+class Song 
+  attr_accessor :name, :artist_name
+  
+  @@all = []
+
+def initialize(name)
+    @name = name 
+    save 
+  end 
+  
+  def save
+    self.class.all << self  
+  end 
+  
+  def self.all 
+    @@all  
+  end 
+  
+  def self.clear_all
+    @@all.clear
+  end
+  
+  def self.print_all 
+    @@all.each do |dogs|
+      puts dogs.name
+  end
+  
+  def save
+        @@all << self
+    end
+  end
+end 
