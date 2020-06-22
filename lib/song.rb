95b1bc340f7ce839318e0b67814b1309ad07a6ab
Shirlen Detablan <slen.detablan@gmail.com>
2020-06-22 20:36:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,7 +33,7 @@ class Song
   end 
   
   def self.find_or_create_by_name
-    @@all.find(name) || self.create(name) 
+    self.find_by_name(name) || self.create_by_name(name)
   end 
   
   def self.new_from_filename(name)
